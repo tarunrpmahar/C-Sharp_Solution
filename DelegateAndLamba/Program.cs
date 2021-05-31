3ca7d857533aadbc450fb2f6fb862e5243f2422f
tarunrpmahar <tsmahar1997@gmail.com>
2021-05-31 17:14:53 +0530

    Add project files.

---
+++ DelegateAndLamba/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DelegateAndLamba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOpDelegate f = Add;
+            //f = Substract;
+
+            Console.WriteLine(f(21, 21));
+
+            CalculateAndPrint(21, 21, Substract);
+
+            CalculateAndPrint(21, 21, delegate (int x, int y) { return (x + y); });
+
+            //lambdas 
+            CalculateAndPrint(21, 21, (int x, int y) => x - y);
+            // OR
+            CalculateAndPrint(21, 21, (x, y) => x * y);
+        }
+
+        delegate int MathOpDelegate(int x, int y);
+
+        static void CalculateAndPrint(int x, int y, MathOpDelegate f)
+        {
+            Console.WriteLine(f(x, y));
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Substract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ PartialClass/TestCoords.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PartialClass
+{
+    public partial class Coords
+    {
+        private int x;
+        private int y;
+
+        public Coords(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public partial class Coords
+    {
+        public void PrintCoords()
+        {
+            Console.WriteLine("Coords: {0},{1}", x, y);
+        }
+    }
+
+    class TestCoords
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Coords myCoords = new Coords(10, 15);
+            myCoords.PrintCoords();
+        }
+    }
+}
